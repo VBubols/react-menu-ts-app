f2afee50798360f128f9b90218618c54d80b54d4
VBubols <vitorbubols@gmail.com>
2026-03-24 21:49:18 -0300

    [VITE] first commit

---
+++ src/pages/DisciplinasPage.tsx
@@ -0,0 +1,33 @@
+export default function DisciplinasPage(){
+    return(
+        <section>
+            <div className="profile-box">
+                <p><strong>Nome da disciplina:</strong> Banco de Dados</p>
+                <p><strong>Professor: Sérgio Alexandre de Araujo</strong></p>
+            </div>
+            <div className="profile-box">
+                <p><strong>Nome da disciplina:</strong> Fundamentos da Eletroeletrônica Aplicada</p>
+                <p><strong>Professor: Bruno Vichinheski</strong></p>
+            </div>
+            <div className="profile-box">
+                <p><strong>Nome da disciplina:</strong> Introdução ao desenvolvimento de projetos</p>
+                <p><strong>Professor: Bruno Pedroso Lima da Silva</strong></p>
+            </div>
+            <div className="profile-box">
+                <p><strong>Nome da disciplina:</strong> Introdução à Tecnologia da Informação e Comunicação</p>
+                <p><strong>Professor:</strong> Bruno Pedroso Lima da Silva</p>
+            </div>
+            <div className="profile-box">
+                <p><strong>Nome da disciplina:</strong> Lógica de Programação</p>
+                <p><strong>Professor: Dennis Jean</strong></p>
+            </div><div className="profile-box">
+                <p><strong>Nome da disciplina:</strong> Modelagem de Sistemas</p>
+                <p><strong>Professor: Bruno Hoffmann</strong></p>
+            </div>
+            <div className="profile-box">
+                <p><strong>Nome da disciplina:</strong> Desenvolvimento de Aplicativos</p>
+                <p><strong>Professor: Marcos Vinicius Silva Coelho</strong></p>
+            </div>
+        </section>
+    )
+}
